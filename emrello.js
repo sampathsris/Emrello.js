09d0e17d23965bceec477b1ff8f90de0128ba250
Sampath Sitinamaluwa <sampathsris@gmail.com>
2015-07-09 23:55:33 +0530

    Improved and refactored code

--- emrello.js
+++ emrello.js
@@ -2,53 +2,120 @@
 (function () {
 	'use strict';
 	var API = 'https://api.trello.com/1/';
+	var PREFIX = 'data-emrello-';
+	var emrello = [];
 	
-	// find the script element containing Emrello.js
-	var emrelloScript = document.querySelector('script[data-emrello-key]');
-	if (!emrelloScript) return;
-	
-	// extract the key
-	var key = emrelloScript.getAttribute('data-emrello-key');
-	if (!key) return;
-	
-	// get embedded cards, lists, and boards
-	var embeddings = document.querySelectorAll('div[data-emrello-type]');
-	if (embeddings.length === 0) return;
-	
-	embeddings:
-	for (var i = 0; i < embeddings.length; i++) {
-		var type = embeddings[i].getAttribute('data-emrello-type');
-		var id = embeddings[i].getAttribute('data-emrello-id');
-		if (!type) type = 'card';
-		var component;
+	/**
+	 * Gets the Trello API key which is embedded as an attribute
+	 * of the script tag which loads Emrello.js
+	 */
+	function getKey() {
+		// find the script element containing Emrello.js
+		var emrelloScript = document.querySelector('script[' + PREFIX + 'key]');
+		if (!emrelloScript) return false;
 		
-		type:
-		switch (type) {
-			case 'card':
-			case 'c':
-				component = 'cards';
-				break type;
-			case 'list':
-			case 'l':
-				component = 'lists';
-				break type;
-			case 'board':
-			case 'b':
-				component = 'boards';
-				break type;
-			default:
-				console.log('unknown type: ' + type);
-				continue embeddings;
+		// extract the key
+		emrello.key = emrelloScript.getAttribute(PREFIX + 'key');
+		if (!emrello.key) return false;
+		
+		return true;
+	}
+	
+	/**
+	 * Represents an embedded Trello object
+	 */
+	function Emrello(element) {
+		var type, id, url;
+		
+		this.prototype = element;
+		
+		/**
+		 * Lazily gets the type of the Emrello, which could be either card, list, or board.
+		 */
+		this.getType = function () {
+			if (!type) getTypeImpl();
+			return type;
+		};
+		
+		/**
+		 * Gets the type of the Emrello.
+		 */
+		function getTypeImpl() {
+			type = element.getAttribute(PREFIX + 'type');
+			
+			if (type == 'c' || type == 'card' || type == null || type == '') {
+				// card is identified by 'c' or 'card'
+				// no value will be interpreted as 'card'
+				type = 'card';
+			} else if (type == 'l' || type == 'list') {
+				// list is identified by 'l' or 'list'
+				type = 'list';
+			} else if (type == 'b' || type == 'board') {
+				// board is identified by 'b' or 'board'
+				type = 'board';
+			}
 		}
 		
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState == 4 && xhr.status == 200) {
-				console.log(type + ': ' + id);
-				console.log(JSON.parse(xhr.responseText));
+		/**
+		 * Gets the object identifier of the Emrello.
+		 */
+		this.getId = function () {
+			if (!id) {
+				id = element.getAttribute(PREFIX + 'id');
 			}
+			
+			return id;
 		};
-		xhr.open('GET', API + component + '/' + id + '?key=' + key, true);
-		xhr.send(null);
+		
+		/**
+		 * Gets the Trello API URL of the Emrello.
+		 */
+		this.getUrl = function () {
+			if (!url) {
+				url = API + this.getType() + 's/' + this.getId() + '?key=' + emrello.key;
+			}
+			
+			return url;
+		};
+		
+		/**
+		 * Causes the Emrello to update by retrieving latest JSON from Trello API
+		 */
+		this.update = function () {
+			var em = this;
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function () {
+				if (this.readyState === 4 && this.status === 200) {
+					render.call(em, this.response);
+				}
+			};
+			xhr.open('GET', this.getUrl(), true);
+			xhr.responseType = 'json';
+			xhr.send(null);
+		};
+		
+		/**
+		 * Renders the Emrello object in the DOM
+		 */
+		function render(data) {
+			console.log('Rendering ' + this.getType() + ' ' + this.getId());
+			this.prototype.innerHTML = this.getType() + ' ' + this.getId();
+		}
+		
+		return this;
+	}
+	
+	if (!getKey()) return;
+	
+	// get embedded cards, lists, and boards
+	var embeddings = document.querySelectorAll('div[' + PREFIX + 'type]');
+	if (embeddings.length === 0) return;
+	
+	for (var i = 0; i < embeddings.length; i++) {
+		emrello.push(new Emrello(embeddings[i]));
+	}
+	
+	for (var j = 0; j < emrello.length; j++) {
+		emrello[j].update();
 	}
 }());
