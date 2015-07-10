03e35ed34732a3e85a3740bb9516ce3be44a3762
sampathsris <sampathsris@gmail.com>
2015-07-11 01:07:29 +0530

    Added styles and templates for cards

--- emrello.js
+++ emrello.js
@@ -3,6 +3,227 @@
 	'use strict';
 	var API = 'https://api.trello.com/1/';
 	var PREFIX = 'data-emrello-';
+	
+	/**
+	 * Styles used on Emrello's
+	 */
+	var STYLES = {
+		'card': {
+			'overflow': 'auto',
+			'padding': '6px 8px 4px',
+			'position': 'relative',
+			'display': 'inline-block',
+			'cursor': 'pointer',
+			'color': '#4d4d4d',
+			'fontFamily': '"Helvetica Neue", Arial, Helvetica, sans-serif',
+			'fontSize': '14px',
+			'lineHeight': '18px',
+			'border': '1px solid #ccc',
+			'borderRadius': '4px',
+			'width': '244px',
+			'textAlign': 'left'
+		},
+		'labels': {
+			'margin': '2px 0',
+			'overflow': 'auto',
+			'position': 'relative'
+		},
+		'label': {
+			'cssFloat': 'left',
+			'height': '8px',
+			'margin': '0 3px 3px 0',
+			'padding': '0',
+			'width': '42px',
+			'lineHeight': '100px',
+			'borderRadius': '3px',
+			'display': 'block',
+			'overflow': 'hidden',
+			'position': 'relative'
+		},
+		'label-green': {
+			'color': '#61bd4f',
+			'backgroundColor': '#61bd4f'
+		},
+		'label-yellow': {
+			'color': '#f2d600',
+			'backgroundColor': '#f2d600'
+		},
+		'label-orange': {
+			'color': '#ffab4a',
+			'backgroundColor': '#ffab4a'
+		},
+		'label-red': {
+			'color': '#eb5a46',
+			'backgroundColor': '#eb5a46'
+		},
+		'label-purple': {
+			'color': '#c377e0',
+			'backgroundColor': '#c377e0'
+		},
+		'label-blue': {
+			'color': '#0079bf',
+			'backgroundColor': '#0079bf'
+		},
+		'label-sky': {
+			'color': '#00c2e0',
+			'backgroundColor': '#00c2e0'
+		},
+		'label-lime': {
+			'color': '#51e898',
+			'backgroundColor': '#51e898'
+		},
+		'label-pink': {
+			'color': '#ff80ce',
+			'backgroundColor': '#ff80ce'
+		},
+		'label-black': {
+			'color': '#4d4d4d',
+			'backgroundColor': '#4d4d4d'
+		},
+		'card-title': {
+			'color': '#4d4d4d',
+			'clear': 'both',
+			'display': 'block',
+			'fontWeight': '400',
+			'margin': '0 0 4px',
+			'overflow': 'hidden',
+			'textDecoration': 'none',
+			'wordWrap': 'break-word',
+			'backgroundColor': 'transparent'
+		},
+		'badges': {
+			'cssFloat': 'left',
+			'paddingBottom': '2px'
+		},
+		'badge': {
+			'color': '#8c8c8c',
+			'display': 'inline-block',
+			'height': '18px',
+			'margin': '0 0 4px 0',
+			'position': 'relative',
+			'textDecoration': 'none',
+			'verticalAlign': 'top',
+			'backgroundColor': '#eee',
+			'borderRadius': '2px',
+			'padding': '0 4px 0 4px'
+		},
+		'badge-text': {
+			'height': '18px',
+			'fontSize': '12px',
+			'lineHeight': '18px',
+			'width': '18px',
+			'textAlign': 'center',
+			'textDecoration': 'none'
+		},
+		'members': {
+			'cssFloat': 'right',
+			'margin': '2px -8px -4px 0px',
+		},
+		'member': {
+			'cssFloat': 'right',
+			'backgroundColor': '#d6dadc',
+			'borderRadius': '3px',
+			'color': '#a9a9a9',
+			'cursor': 'pointer',
+			'display': 'block',
+			'height': '30px',
+			'width': '30px',
+			'margin': '0 4px 4px 0',
+			'overflow': 'hidden',
+			'position': 'relative',
+			'textDecoration': 'none',
+			'lineHeight': '30px',
+			'fontWeight': 'bold',
+			'fontSize': '18px',
+			'textAlign': 'center'
+		}
+	};
+	
+	/**
+	 * Emrello templates
+	 */
+	var TEM_LABEL = {
+		'type': 'span',
+		'styles': [ 'label' ],
+		'attributes': {
+			'title': function () {}
+		},
+		'content': function () {}
+	};
+	var TEM_LABEL_GROUP = {
+		'type': 'div',
+		'styles': [ 'labels' ],
+		'content': function () {}
+	};
+	var TEM_CARD_TITLE = {
+		'type': 'a',
+		'styles': [ 'card-title' ],
+		'attributes': {
+			'href': function () {},
+			'target': '_blank'
+		},
+		'content': function () {}
+	};
+	var TEM_BADGE = {
+		'type': 'div',
+		'styles': [ 'badge' ],
+		'attributes': {
+			'title': function () {}
+		},
+		'content': [
+			{
+				'type': 'span',
+				'styles': [ 'badge-text' ],
+				'content': function () {}
+			}
+		]
+	};
+	var TEM_BADGE_GROUP = {
+		'type': 'div',
+		'styles': [ 'badges' ],
+		'content': function () {}
+	};
+	var TEM_MEMBER = {
+		'type': 'div',
+		'styles': [ 'member' ],
+		'attributes': {
+			'title': function () {}
+		},
+		'content': function () {}
+	};
+	var TEM_MEMBER_GROUP = {
+		'type': 'div',
+		'styles': [ 'members' ],
+		'content': function () {}
+	};
+	var TEM_CARD = {
+		'type': 'div',
+		'styles': [ 'card' ],
+		'content': [
+			TEM_LABEL_GROUP,
+			TEM_CARD_TITLE,
+			TEM_BADGE_GROUP,
+			TEM_MEMBER_GROUP
+		]
+	};
+	var TEM_DUMMY = {
+		'type': 'span',
+		'styles': [],
+		'content': function () {}
+	}
+	
+	/**
+	 * Root template belonging to each Emrello type
+	 */
+	var TEMPLATE_TYPES = {
+		'card': TEM_CARD,
+		'list': TEM_DUMMY,
+		'board': TEM_DUMMY
+	};
+	
+	/**
+	 * Holds the list of Emrello's
+	 */
 	var emrello = [];
 	
 	/**
@@ -19,6 +240,14 @@
 		if (!emrello.key) return false;
 		
 		return true;
+	}
+	
+	/**
+	 * Renders a given template inside a given parent element
+	 * using a given data object
+	 */
+	function renderTemplate(parent, template, data) {
+		parent.innerHTML = 'Rendering template';
 	}
 	
 	/**
@@ -111,6 +340,8 @@
 		 * Renders the Emrello object in the DOM
 		 */
 		function render(data) {
+			var template = TEMPLATE_TYPES[this.getType()];
+			renderTemplate(this.prototype, template, data);
 		}
 		
 		return this;
